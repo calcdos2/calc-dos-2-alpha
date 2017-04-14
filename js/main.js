e4514561995ede5a34b7686193d3506dfb7705da
calcdos2 <nikshes.ru@gmail.com>
2017-04-14 19:04:58 +0300

    Added tooltips and level needed + new styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+$(document).ready(function(){
+var db = JSON.parse(data);
+	$(".tooltiptest").attr("data-toggle","tooltip")
+	$(".tooltiptest").attr("data-html","true")
 
+	$(".tooltiptest").mouseover(function(event){
+	var elementIdTooltip = event.target.id
+
+	 tooltipInfo (elementIdTooltip, db)
+	})
+
+	$(".tooltiptest").mouseout(function(event, elementIdTooltip){
+	var elementIdTooltip = event.target.id
+	tooltipRemove (elementIdTooltip)
+	})
+
+})
 
 
 $("body").click (function getElemetId(event){
@@ -32,6 +48,7 @@ idSearch (elementId , setId)
 toggleTalentsColor (elementId)
 sumCombatSKills()
 sumCivilSkills ()
+setLevel ()
 }
 
 return 
@@ -199,4 +216,194 @@ if ($(".resetClass").hasClass("talentResetClass")){
 }
 
 	return
+}
+
+
+function tooltipInfo (elementIdTooltip, db) {
+	var value1 = db[elementIdTooltip].value1
+	var value2 = db[elementIdTooltip].value2
+	var desc1 = "%"+" " + db[elementIdTooltip].desc1 
+	var desc2 = "%"+" " + db[elementIdTooltip].desc2
+	var mainDesc =  " " +db[elementIdTooltip].descMain
+	if(elementIdTooltip == db[elementIdTooltip].idname){
+		
+		if ($("#counter"+elementIdTooltip).hasClass("attribute")){
+			var numberValue = parseInt($("#counter"+elementIdTooltip).text())-10;
+			var fullDescription
+			
+			if(db[elementIdTooltip].desc2 === ""){
+				fullDescription = numberValue * value1 + desc1 +"<br>" + mainDesc
+				
+				if(db[elementIdTooltip].idname == "Mem"){
+					
+					desc1 =" "+ db[elementIdTooltip].desc1
+					fullDescription = numberValue * value1 + desc1+"<br>" + mainDesc
+				}
+				if(db[elementIdTooltip].idname == "Persuasion"){
+					
+					fullDescription =  mainDesc
+				}
+				
+			}
+			else{	
+
+					if(db[elementIdTooltip].idname == "Wit"){
+
+						desc2 = " " + db[elementIdTooltip].desc2
+						fullDescription = numberValue * value1 + desc1+"<br>"+ numberValue * value2+ desc2 +"<br>"+ mainDesc
+					}
+					else{
+						fullDescription = numberValue * value1 + desc1 +"<br>"+ numberValue * value2+ desc2+"<br>" + mainDesc
+					}
+	    		}
+    	}
+   		
+
+   		if($("#"+elementIdTooltip).hasClass("talent")){
+    	var fullDescription = mainDesc
+
+    	}
+    	
+
+
+    	if($("#counter"+elementIdTooltip).hasClass("skillsResetClass")){
+    		var numberValue = parseInt($("#counter"+elementIdTooltip).text());
+    		var fullDescription
+    		if(db[elementIdTooltip].desc2 === ""){
+				fullDescription = numberValue * value1 + desc1+"<br>" + mainDesc
+				if(db[elementIdTooltip].desc1 === ""){
+					fullDescription = mainDesc
+				}
+			}
+			else{	
+				if (db[elementIdTooltip].idname == "Summon" || db[elementIdTooltip].idname == "Necr"){
+					
+						desc1 = db[elementIdTooltip].desc1 
+						desc2 = db[elementIdTooltip].desc2
+						fullDescription = desc1+" "+ numberValue * value1+"%"+" " +desc2 +"<br>"+ mainDesc
+					}
+				else{					
+	    		fullDescription = numberValue * value1 + desc1 +"<br>"+ numberValue * value2+ desc2 +"<br>"+ mainDesc
+	    		}
+    		}
+    	}
+
+    $("#"+elementIdTooltip).attr("data-original-title", fullDescription)
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    return
+}
+}
+
+function tooltipRemove (elementIdTooltip) {
+    	$("#"+elementIdTooltip).removeAttr("title")
+    return
+}
+
+
+function setLevel (){
+	var levelAttr = setLevelAttr()
+	var levelCombat =  setLevelCombatSkills()
+	var levelCivil = setLevelCivilSkills()
+	var levelTalents = setLevelTalents()
+	var setLevelCount =  Math.max(levelAttr, levelCombat, levelCivil, levelTalents)
+	$("#counterLevel").text(setLevelCount)
+
+	if (levelAttr+levelTalents+levelCivil+levelCombat==0){
+		$("#counterLevel").text(0)
+	}
+	return
+}
+
+
+function setLevelAttr (){
+	var totalAttr = parseInt($("#counterTotalAttr").text())
+	var levelAttr = 0
+
+	if (totalAttr >= 0 && totalAttr <=3){
+	levelAttr = 1
+	
+	}
+	if (totalAttr > 3){
+		levelAttr=1
+		var i = totalAttr
+		for (i;i>3;i=i-2){
+			levelAttr = levelAttr+1
+		}
+	}
+	if (totalAttr == 0 ){
+		levelAttr = 0
+	}
+	return levelAttr
+}
+
+function setLevelCombatSkills (){
+	var totalCombatSkills = parseInt($("#counterTotalCombatSkills").text())
+	var levelCombat = 0; 
+
+	if (totalCombatSkills >= 0 && totalCombatSkills <=2){
+	levelCombat = 1
+	
+	}
+	if (totalCombatSkills > 2){
+		var i = totalCombatSkills
+		for (i;i>1;i=i-1){
+			levelCombat = levelCombat+1
+		}
+	}
+	if (totalCombatSkills == 0 ){
+		levelCombat = 0
+	}
+	return levelCombat
+}
+
+function setLevelCivilSkills (){
+	var totalCivilSkills = parseInt($("#counterTotalCivilSkills").text())
+	var levelCivil=0; 
+
+	if (totalCivilSkills >= 0 && totalCivilSkills <=1){
+	levelCivil = 1
+	
+	}
+	if (totalCivilSkills ==2){
+	levelCivil = 2
+	
+	}
+	if (totalCivilSkills > 2){
+		levelCivil = 2 
+		var i = totalCivilSkills
+		for (i;i>2;i=i-1/4){
+			
+			levelCivil= levelCivil+1
+		}
+	}
+	if (totalCivilSkills == 0 ){
+		levelCivil = 0
+	}
+	return levelCivil
+}
+
+function setLevelTalents (){
+	var count = $(".setActive")
+	var levelTalents = 0
+	if (count.length >= 0 && count.length <=1){
+	levelTalents = 1
+	
+	}
+	if (count.length ==2){
+	levelTalents = 4
+	}	
+
+	if (count.length > 2){
+		levelTalents = 3 
+		var i = count.length
+		for (i;i>2;i=i-1/6){
+			
+			levelTalents= levelTalents+1	
+		}
+	}
+	if (count.length== 0 ){
+		levelTalents = 0
+	}
+	return levelTalents
 }
